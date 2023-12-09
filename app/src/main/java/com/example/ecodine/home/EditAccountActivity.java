d39f1555604345ebc852b4d40325317a3f6300c7
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-09 15:55:01 +0200

    finish desgin screens

---
+++ app/src/main/java/com/example/ecodine/home/EditAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecodine.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecodine.R;
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account);
+    }
+}
